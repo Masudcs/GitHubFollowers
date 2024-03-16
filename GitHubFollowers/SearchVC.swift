6549a74b8abe748092a320c6c7e0a3b3969d6f6d
Md. Masud Rana <masud.pust16@gmail.com>
2024-03-16 09:37:25 +0600

    initial commit

---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,30 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Md. Masud Rana on 3/14/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
